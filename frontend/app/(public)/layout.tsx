781ebe32c28cea47aff2fa8a9ec791ec1c0fca9e
Joenel Sevellejo <joenelsevellejo831@gmail.com>
2026-04-30 22:42:25 +0800

    feat: frontend / with proper setup for main layout

---
+++ frontend/app/(public)/layout.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import React from 'react';
+import {useRouter} from 'next/navigation';
+
+export default function PublicLayout({children}: {children: React.ReactNode}) {
+  const router = useRouter();
+
+  return (
+    <>
+      <div>
+        <button onClick={() => router.push('/')}>Sign in</button>
+      </div>
+      <main className="min-h-screen bg-amber-50">{children}</main>
+      <span>Footer</span>
+    </>
+  );
+}
