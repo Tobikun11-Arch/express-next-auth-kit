e7f615dbfa2cc4906d16636d7a5bb69d84255dd1
Joenel <joenelsevellejo831@gmail.com>
2026-05-02 18:26:44 +0800

    tanstack parent provider fix

--- frontend/lib/provider/ReactQueryProvider.tsx
+++ frontend/lib/provider/ReactQueryProvider.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {ReactNode, useState} from 'react';
 
@@ -5,8 +7,9 @@ type ReactQueryProviderProps = {
   children: ReactNode;
 };
 
-export function ReactQueryProvider({children}: ReactQueryProviderProps) {
+export default function ReactQueryProvider({children}: ReactQueryProviderProps) {
   const [queryClient] = useState(() => new QueryClient());
+
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
