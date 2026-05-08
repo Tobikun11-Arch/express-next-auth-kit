d345c096eb52c67cda880f19fe6c89bb4a8f83ab
Joenel Sevellejo <joenelsevellejo831@gmail.com>
2026-05-08 22:42:31 +0800

    added: origin deployed url

--- backend/index.ts
+++ backend/index.ts
@@ -13,7 +13,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'your-production-domain.com'],
+    origin: ['http://localhost:3000', 'https://express-next-auth-kit.vercel.app/'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
